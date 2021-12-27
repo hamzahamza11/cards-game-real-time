35be3dd6d7b1fc1b1702db79885b840dd81138e2
hamazSabir <sabirha00@gmail.com>
2021-12-27 06:07:14 +0100

    reaady to deploy 14

--- app.js
+++ app.js
@@ -74,9 +74,10 @@ io.on("connection", (socket) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
+  app.use(express.static('./frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 8080;
