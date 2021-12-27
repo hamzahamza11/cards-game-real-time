7d1ea72c7eefa0040c89e17b91d512ce46a64bc3
hamazSabir <sabirha00@gmail.com>
2021-12-27 01:28:48 +0100

    reaady to deploy 2

--- app.js
+++ app.js
@@ -71,6 +71,24 @@ io.on("connection", (socket) => {
   })
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('./frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,(err)=>{
+  if(err){
+      console.log(err);
+  }
+  console.log("app is running"+PORT);
+})
+
 server.listen(8080, () => {
   console.log("listening on *:8080");
 });
